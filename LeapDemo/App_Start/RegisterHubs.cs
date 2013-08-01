784227056d7972e663b6f6349dad687bd8b3c542
David Miranda <davidmiranda11@gmail.com>
2013-08-01 02:37:54 +0100

    first commit

---
+++ LeapDemo/App_Start/RegisterHubs.cs
@@ -0,0 +1,15 @@
+using System.Web.Routing;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(SignalR.StockTicker.RegisterHubs), "Start")]
+
+namespace SignalR.StockTicker
+{
+    public static class RegisterHubs
+    {
+        public static void Start()
+        {
+            // Register the default hubs route: ~/signalr
+            RouteTable.Routes.MapHubs();
+        }
+    }
+}
---
+++ LeapDemo/DemoHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace LeapDemo
+{
+    public class DemoHub : Hub
+    {
+        public void Send(string frameData)
+        {
+            Clients.All.recieveData(frameData);
+            var frame = JsonConvert.DeserializeObject<dynamic>(frameData);
+            foreach (var gesture in frame.gestures)
+            {
+                if (gesture.type == "circle" && gesture.state == "stop")
+                {
+                    Clients.All.recieveCircle();
+                }
+            }
+        }
+    }
+}
